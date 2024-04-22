020be53c882f84069b1fa7febe593ad705870f1a
Chris Straw <chrisstraw@users.noreply.github.com>
2024-04-22 15:45:09 -0400

    Initial Project Commit (#1)

---
+++ src/OLT.DataAdapters.AutoMapper/Interfaces/IOltAdapterPagedMap.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace OLT.Core
+{
+    public interface IOltAdapterPagedMap<TEntity, TDestination> : IOltAdapterMap<TEntity, TDestination>, IOltAdapter
+    {
+        IOrderedQueryable<TEntity> DefaultOrderBy(IQueryable<TEntity> queryable);
+    }
+}
---
+++ src/OLT.Extensions.DependencyInjection.AutoMapper/OltAutoMapperAssemblyFilter.cs
@@ -0,0 +1,14 @@
+﻿namespace OLT.Core
+{
+    public class OltAutoMapperAssemblyFilter : OltAssemblyFilter
+    {
+        /// <summary>
+        /// OLT.*, MyApp.*, and includes exclustions <seealso cref="OltAssemblyFilter.WithDefaultDIExclusionFilters"/>
+        /// </summary>
+        /// <param name="filters"></param>        
+        public OltAutoMapperAssemblyFilter(params string[] filters) : base(filters)
+        {
+            base.WithDefaultDIExclusionFilters();
+        }
+    }
+}
