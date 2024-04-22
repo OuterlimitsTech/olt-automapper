69161c464a390facc06d57401f658f955871e055
Chris Straw <chris.straw@outerlimitstech.com>
2024-04-22 15:43:47 -0400

    Initial Project Commit

---
+++ tests/OLT.DataAdapters.AutoMapper.Tests/Assets/Models/AdapterObject9.cs
@@ -0,0 +1,11 @@
+﻿using OLT.Core;
+
+namespace OLT.DataAdapters.AutoMapper.Tests.Assets.Models
+{
+    public class AdapterObject9 : IAdapterObject
+    {
+        public int ObjectId { get; set; }
+        public string? StreetAddress { get; set; }
+        public OltPersonName? Name { get; set; }
+    }
+}
